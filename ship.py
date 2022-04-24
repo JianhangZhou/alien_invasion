a205b1e26659e17f53413bad6f48d9dd4eccee6b
Jianhang Zhou <55510134+JianhangZhou@users.noreply.github.com>
2022-04-23 17:21:00 -0700

    update scripts

--- ship.py
+++ ship.py
@@ -5,19 +5,28 @@ class Ship:
 	def __init__(self, ai_game):
 		""" Initialize ship and position """
 		self.screen = ai_game.screen
+		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
 
 		# Load ship image and get rect
 		self.image = pygame.image.load('images/ship.bmp')
 		self.rect = self.image.get_rect()
+		self.x = float(self.rect.x)
 
 		# Position
 		self.rect.midbottom = self.screen_rect.midbottom
 		self.moving_right = False
+		self.moving_left = False
+
+		# Setting
+		self.ship_speed = 1.5
 
 	def update(self):
-		if self.moving_right:
-			self.rect.x += 1
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > 0:
+			self.x -= self.settings.ship_speed
+		self.rect.x = self.x
 
 	def blitme(self):
 		""" Render new ship """
